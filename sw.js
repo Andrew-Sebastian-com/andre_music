9b7b24535241386ad2a964659c9dcd308b9a678b
Andrew-Sebastian-com <andrew.sbstian@gmail.com>
2026-08-13 19:19:32 +0700

    Maybe

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'andre-music-v1';
+const ASSETS = [
+  '/andre_music/',
+  '/andre_music/index.html',
+  '/andre_music/logo.png'
+];
+
+// Install event - Caches core assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate event - Cleans up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event - Serves assets from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
